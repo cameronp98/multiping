d9c1be25329888a97a0994f32b341604eb8eec18
Cameron Phillips <cameron0505@gmail.com>
2019-07-29 06:15:54 +0100

    Update client binary

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,6 +9,9 @@ fn main() {
     debug!("Client main:");
 
     let client = Client::new("127.0.0.1:3000");
-    let resp = client.send(Message::Ping).unwrap();
-    println!("{:?}", resp);
+    // let resp = client.send(Message::Ping).unwrap();
+    println!("{:?}", client.send(Message::Pong));
+    println!("{:?}", client.send(Message::Ping));
+    println!("{:?}", client.send(Message::Text(String::from("Hello, World!"))));
+    println!("{:?}", client.send(Message::InvalidRequest));
 }
